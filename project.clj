87455f96884b38a0f1fd3ffb9a01e4511f101013
Randy J. Ray <rjray@blackperl.com>
2022-12-10 16:09:06 -0800

    Bump Clojure base version to 1.11.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://adventofcode.com"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/math.combinatorics "0.1.6"]]
   :plugins [[lein-kibit "0.1.6"]]
