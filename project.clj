ee658894f938987982a8d175f782828d1e0ae9bc
Randy J. Ray <rjray@blackperl.com>
2023-11-17 17:42:04 -0800

    Some adjustments to the template file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject advent-of-code "0.1.0-SNAPSHOT"
+(defproject advent-of-code "0.1.0"
   :description "Basis/template for Advent of Code"
-  :url "https://adventofcode.com"
+  :url "https://github.com/rjray/advent-clojure-basis"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.0"]
