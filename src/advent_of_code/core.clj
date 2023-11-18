03a03044bdf62ac0de43949f6f06f0e291361eb0
Randy J. Ray <rjray@blackperl.com>
2023-11-18 11:22:36 -0800

    Rewritten to be faster and cleaner/clearer
    No longer loads all 25 day-modules. Dynamically loads the ns for the
    requested day, resolves the fn for the requested part, and runs the
    resolved sub. Also uses the `time-it` macro from utils to report the
    run-time.

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,84 +1,20 @@
 (ns advent-of-code.core
-  (:require [advent-of-code.utils :as u]
-            [advent-of-code.day01]
-            [advent-of-code.day02]
-            [advent-of-code.day03]
-            [advent-of-code.day04]
-            [advent-of-code.day05]
-            [advent-of-code.day06]
-            [advent-of-code.day07]
-            [advent-of-code.day08]
-            [advent-of-code.day09]
-            [advent-of-code.day10]
-            [advent-of-code.day11]
-            [advent-of-code.day12]
-            [advent-of-code.day13]
-            [advent-of-code.day14]
-            [advent-of-code.day15]
-            [advent-of-code.day16]
-            [advent-of-code.day17]
-            [advent-of-code.day18]
-            [advent-of-code.day19]
-            [advent-of-code.day20]
-            [advent-of-code.day21]
-            [advent-of-code.day22]
-            [advent-of-code.day23]
-            [advent-of-code.day24]
-            [advent-of-code.day25]))
+  (:require [advent-of-code.utils :as u]))
 
 (defn -main
   "Used to dispatch tasks from the command line."
-  [part]
-  (case part
-    "d01.p1" (println (advent-of-code.day01/part-1 (u/read-input "day01.txt")))
-    "d01.p2" (println (advent-of-code.day01/part-2 (u/read-input "day01.txt")))
-    "d02.p1" (println (advent-of-code.day02/part-1 (u/read-input "day02.txt")))
-    "d02.p2" (println (advent-of-code.day02/part-2 (u/read-input "day02.txt")))
-    "d03.p1" (println (advent-of-code.day03/part-1 (u/read-input "day03.txt")))
-    "d03.p2" (println (advent-of-code.day03/part-2 (u/read-input "day03.txt")))
-    "d04.p1" (println (advent-of-code.day04/part-1 (u/read-input "day04.txt")))
-    "d04.p2" (println (advent-of-code.day04/part-2 (u/read-input "day04.txt")))
-    "d05.p1" (println (advent-of-code.day05/part-1 (u/read-input "day05.txt")))
-    "d05.p2" (println (advent-of-code.day05/part-2 (u/read-input "day05.txt")))
-    "d06.p1" (println (advent-of-code.day06/part-1 (u/read-input "day06.txt")))
-    "d06.p2" (println (advent-of-code.day06/part-2 (u/read-input "day06.txt")))
-    "d07.p1" (println (advent-of-code.day07/part-1 (u/read-input "day07.txt")))
-    "d07.p2" (println (advent-of-code.day07/part-2 (u/read-input "day07.txt")))
-    "d08.p1" (println (advent-of-code.day08/part-1 (u/read-input "day08.txt")))
-    "d08.p2" (println (advent-of-code.day08/part-2 (u/read-input "day08.txt")))
-    "d09.p1" (println (advent-of-code.day09/part-1 (u/read-input "day09.txt")))
-    "d09.p2" (println (advent-of-code.day09/part-2 (u/read-input "day09.txt")))
-    "d10.p1" (println (advent-of-code.day10/part-1 (u/read-input "day10.txt")))
-    "d10.p2" (println (advent-of-code.day10/part-2 (u/read-input "day10.txt")))
-    "d11.p1" (println (advent-of-code.day11/part-1 (u/read-input "day11.txt")))
-    "d11.p2" (println (advent-of-code.day11/part-2 (u/read-input "day11.txt")))
-    "d12.p1" (println (advent-of-code.day12/part-1 (u/read-input "day12.txt")))
-    "d12.p2" (println (advent-of-code.day12/part-2 (u/read-input "day12.txt")))
-    "d13.p1" (println (advent-of-code.day13/part-1 (u/read-input "day13.txt")))
-    "d13.p2" (println (advent-of-code.day13/part-2 (u/read-input "day13.txt")))
-    "d14.p1" (println (advent-of-code.day14/part-1 (u/read-input "day14.txt")))
-    "d14.p2" (println (advent-of-code.day14/part-2 (u/read-input "day14.txt")))
-    "d15.p1" (println (advent-of-code.day15/part-1 (u/read-input "day15.txt")))
-    "d15.p2" (println (advent-of-code.day15/part-2 (u/read-input "day15.txt")))
-    "d16.p1" (println (advent-of-code.day16/part-1 (u/read-input "day16.txt")))
-    "d16.p2" (println (advent-of-code.day16/part-2 (u/read-input "day16.txt")))
-    "d17.p1" (println (advent-of-code.day17/part-1 (u/read-input "day17.txt")))
-    "d17.p2" (println (advent-of-code.day17/part-2 (u/read-input "day17.txt")))
-    "d18.p1" (println (advent-of-code.day18/part-1 (u/read-input "day18.txt")))
-    "d18.p2" (println (advent-of-code.day18/part-2 (u/read-input "day18.txt")))
-    "d19.p1" (println (advent-of-code.day19/part-1 (u/read-input "day19.txt")))
-    "d19.p2" (println (advent-of-code.day19/part-2 (u/read-input "day19.txt")))
-    "d20.p1" (println (advent-of-code.day20/part-1 (u/read-input "day20.txt")))
-    "d20.p2" (println (advent-of-code.day20/part-2 (u/read-input "day20.txt")))
-    "d21.p1" (println (advent-of-code.day21/part-1 (u/read-input "day21.txt")))
-    "d21.p2" (println (advent-of-code.day21/part-2 (u/read-input "day21.txt")))
-    "d22.p1" (println (advent-of-code.day22/part-1 (u/read-input "day22.txt")))
-    "d22.p2" (println (advent-of-code.day22/part-2 (u/read-input "day22.txt")))
-    "d23.p1" (println (advent-of-code.day23/part-1 (u/read-input "day23.txt")))
-    "d23.p2" (println (advent-of-code.day23/part-2 (u/read-input "day23.txt")))
-    "d24.p1" (println (advent-of-code.day24/part-1 (u/read-input "day24.txt")))
-    "d24.p2" (println (advent-of-code.day24/part-2 (u/read-input "day24.txt")))
-    "d25.p1" (println (advent-of-code.day25/part-1 (u/read-input "day25.txt")))
-    "d25.p2" (println (advent-of-code.day25/part-2 (u/read-input "day25.txt")))
-    (println "not found")))
-
+  [day part]
+  (let [day   (Integer/parseInt day)
+        part  (Integer/parseInt part)
+        input (format "day%02d.txt" day)
+        sub   (requiring-resolve
+               (symbol (format "advent-of-code.day%02d/part-%d" day part)))]
+    (cond
+      (or (< day 1)
+          (> day 25)) (throw (AssertionError. "Day out of range."))
+      (or (< part 1)
+          (> part 2)) (throw (AssertionError. "Part out of range."))
+      (nil? sub)      (throw (Exception. "Matching fn not found."))
+      :else           (let [[r t] (u/time-it (sub (u/read-input input)))]
+                        (println r)
+                        (println (format "\nTime: %.4fs" t))))))
