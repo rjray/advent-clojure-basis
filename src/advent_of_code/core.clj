d92bf6aa420270a8254e31d02a7275640e662109
Randy J. Ray <rjray@blackperl.com>
2023-11-19 21:14:01 -0800

    Add ability to run "bis" version of days

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -2,13 +2,21 @@
   (:require [advent-of-code.utils :as u]))
 
 (defn -main
-  "Used to dispatch tasks from the command line."
-  [day part]
-  (let [day   (Integer/parseInt day)
-        part  (Integer/parseInt part)
+  "Used to dispatch tasks from the command line.
+
+  Usage: lein run [ -b ] DAY PART
+
+  where DAY and PART are integers selecting the day and part, respectively.
+
+  If -b is given, load the 'bis' version of the day's code."
+  [& args]
+  (let [bis   (= (first args) "-b")
+        day   (Integer/parseInt (if bis (second args) (first args)))
+        part  (Integer/parseInt (if bis (nth args 2) (second args)))
         input (format "day%02d.txt" day)
         sub   (requiring-resolve
-               (symbol (format "advent-of-code.day%02d/part-%d" day part)))]
+               (symbol (format "advent-of-code.day%02d%s/part-%d"
+                               day (if bis "bis" "") part)))]
     (cond
       (or (< day 1)
           (> day 25)) (throw (AssertionError. "Day out of range."))
